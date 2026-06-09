74b4204017d493cd4001ba298a9000057f65bd64
Mohd Saif <saifofficial0778@gmail.com>
2026-06-09 22:39:17 +0530

    React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>🚀 React Logic Lab</h1>
+      <p>Yahan sirf dimaag chalega, CSS nahi!</p>
+      {/* Humare modules yahan render honge ek-ek karke */}
+    </div>
+  );
+}
+
+export default App;
